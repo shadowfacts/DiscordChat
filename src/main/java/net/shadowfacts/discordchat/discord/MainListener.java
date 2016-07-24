797606ec3a2bb5159749155d2545357ec19ac634
Brandon Worl <brandon.worl@originate.com>
2016-07-23 19:06:33 -0700

    Add chat command system and command to see online players

--- src/main/java/net/shadowfacts/discordchat/discord/MainListener.java
+++ src/main/java/net/shadowfacts/discordchat/discord/MainListener.java
@@ -4,6 +4,7 @@ import net.dv8tion.jda.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.hooks.ListenerAdapter;
 import net.shadowfacts.discordchat.DCConfig;
 import net.shadowfacts.discordchat.utils.MiscUtils;
+import net.shadowfacts.discordchat.utils.CommandUtils;
 
 /**
  * @author shadowfacts
@@ -12,9 +13,16 @@ public class MainListener extends ListenerAdapter {
 
 	@Override
 	public void onGuildMessageReceived(GuildMessageReceivedEvent event) {
-		if (MiscUtils.shouldUseChannel(event.getChannel()) && event.getGuild().getId().equals(DCConfig.serverId) && !MiscUtils.isMessageFromMC(event.getMessage())) {
+		if (shouldForwardMessage(event)) {
 			MiscUtils.sendMessage(MiscUtils.fromDiscordMessage(event.getMessage()));
 		}
+	}
+
+	private boolean shouldForwardMessage(GuildMessageReceivedEvent event) {
+		return (MiscUtils.shouldUseChannel(event.getChannel()) &&
+						event.getGuild().getId().equals(DCConfig.serverId) &&
+						!MiscUtils.isMessageFromMC(event.getMessage()) &&
+						!CommandUtils.processCommand(event));
 	}
 
 }
