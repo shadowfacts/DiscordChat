285a84cdea1da4f5d050aa567401ea5575b3c23d
Shadowfacts <me@shadowfacts.net>
2017-01-12 18:03:40 -0500

    Add 1.11.2

---
+++ 1.11.2/src/main/java/net/shadowfacts/discordchat/one_eleven/ForgeEventHandler.java
@@ -0,0 +1,61 @@
+package net.shadowfacts.discordchat.one_eleven;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TextComponentString;
+import net.minecraftforge.common.util.FakePlayer;
+import net.minecraftforge.event.ServerChatEvent;
+import net.minecraftforge.event.entity.living.LivingDeathEvent;
+import net.minecraftforge.event.entity.player.AchievementEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.PlayerEvent;
+
+/**
+ * @author shadowfacts
+ */
+public class ForgeEventHandler {
+
+	@SubscribeEvent
+	public void onServerChat(ServerChatEvent event) {
+		OneElevenTwoMod.discordChat.sendMessage(OneElevenTwoMod.discordChat.getFormatter().fromMC(event.getPlayer().getName(), event.getMessage()));
+	}
+
+	@SubscribeEvent
+	public void onLivingDeath(LivingDeathEvent event) {
+		if (OneElevenTwoMod.config.sendDeathMessages() && event.getEntityLiving() instanceof EntityPlayer && !(event.getEntityLiving() instanceof FakePlayer) && !event.getEntity().world.isRemote) {
+			EntityPlayer player = (EntityPlayer)event.getEntityLiving();
+			OneElevenTwoMod.discordChat.sendMessage(OneElevenTwoMod.discordChat.getFormatter().death(player.getName(), player.getCombatTracker().getDeathMessage().getUnformattedText()));
+		}
+	}
+
+	@SubscribeEvent
+	public void onPlayerReceiveAchievement(AchievementEvent event) {
+		if (OneElevenTwoMod.config.sendAchievementMessages() && event.getEntityPlayer() instanceof EntityPlayerMP) {
+			if (((EntityPlayerMP) event.getEntityPlayer()).getStatFile().hasAchievementUnlocked(event.getAchievement())) {
+				return;
+			}
+			if (!((EntityPlayerMP) event.getEntityPlayer()).getStatFile().canUnlockAchievement(event.getAchievement())) {
+				return;
+			}
+			ITextComponent achievementComponent = event.getAchievement().getStatName();
+			ITextComponent achievementText = new TextComponentString("[").appendSibling(achievementComponent).appendText("]");
+			OneElevenTwoMod.discordChat.sendMessage(OneElevenTwoMod.discordChat.getFormatter().achievement(event.getEntityPlayer().getName(), achievementText.getUnformattedText()));
+		}
+	}
+
+	@SubscribeEvent
+	public void onPlayerLoggedIn(PlayerEvent.PlayerLoggedInEvent event) {
+		if (OneElevenTwoMod.config.sendJoinLeaveMessages()) {
+			OneElevenTwoMod.discordChat.sendMessage(OneElevenTwoMod.discordChat.getFormatter().join(event.player.getName()));
+		}
+	}
+
+	@SubscribeEvent
+	public void onPlayerLoggedOut(PlayerEvent.PlayerLoggedOutEvent event) {
+		if (OneElevenTwoMod.config.sendJoinLeaveMessages()) {
+			OneElevenTwoMod.discordChat.sendMessage(OneElevenTwoMod.discordChat.getFormatter().leave(event.player.getName()));
+		}
+	}
+
+}
---
+++ 1.11.2/src/main/java/net/shadowfacts/discordchat/one_eleven/OneElevenTwoAdapter.java
@@ -0,0 +1,112 @@
+package net.shadowfacts.discordchat.one_eleven;
+
+import com.mojang.authlib.GameProfile;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.management.PlayerList;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.text.TextComponentString;
+import net.minecraft.world.WorldServer;
+import net.minecraft.world.storage.IPlayerFileData;
+import net.minecraftforge.common.DimensionManager;
+import net.minecraftforge.common.ForgeHooks;
+import net.minecraftforge.common.util.FakePlayer;
+import net.minecraftforge.common.util.FakePlayerFactory;
+import net.minecraftforge.fml.common.FMLCommonHandler;
+import net.shadowfacts.discordchat.api.IMinecraftAdapter;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+/**
+ * @author shadowfacts
+ */
+public class OneElevenTwoAdapter implements IMinecraftAdapter {
+
+	@Override
+	public void sendMessage(String message) {
+		FMLCommonHandler.instance().getMinecraftServerInstance().getPlayerList().sendMessage(ForgeHooks.newChatWithLinks(message));
+	}
+
+	@Override
+	public int[] getAllDimensions() {
+		Integer[] boxed = DimensionManager.getIDs();
+		int[] unboxed = new int[boxed.length];
+		for (int i = 0; i < boxed.length; i++) {
+			unboxed[i] = boxed[i];
+		}
+		return unboxed;
+	}
+
+	@Override
+	public double getTickTime(int dimension) {
+		MinecraftServer server = FMLCommonHandler.instance().getMinecraftServerInstance();
+		long[] tickTimes = server.worldTickTimes.get(dimension);
+		long sum = 0;
+		for (int i = 0; i < tickTimes.length; i++) {
+			sum += tickTimes[i];
+		}
+		return sum / tickTimes.length * 1.0E-6D;
+	}
+
+	@Override
+	public Set<String> getOnlinePlayers() {
+		return FMLCommonHandler.instance().getMinecraftServerInstance().getPlayerList().getPlayers().stream()
+				.map(EntityPlayer::getName)
+				.collect(Collectors.toSet());
+	}
+
+	@Override
+	public void executeCommand(String command) {
+		FMLCommonHandler.instance().getMinecraftServerInstance().callFromMainThread(() -> {
+			FMLCommonHandler.instance().getMinecraftServerInstance().getCommandManager().executeCommand(DummySender.INSTANCE, command);
+			return null;
+		});
+	}
+
+	@Override
+	public String teleportPlayerToSpawn(String username) {
+		MinecraftServer server = FMLCommonHandler.instance().getMinecraftServerInstance();
+		WorldServer world = server.worldServerForDimension(0);
+		BlockPos spawn = world.getSpawnPoint();
+		PlayerList playerList = server.getPlayerList();
+		EntityPlayerMP player = playerList.getPlayerByUsername(username);
+		if (player != null) {
+			if (player.dimension != 0) {
+				int original = player.dimension;
+				playerList.transferPlayerToDimension(player, 0, new DummyTeleporter(world));
+
+				if (original == 1 && player.isEntityAlive()) {
+					world.spawnEntity(player);
+					world.updateEntityWithOptionalForce(player, false);
+				}
+			}
+			player.setPositionAndUpdate(spawn.getX() + 0.5d, spawn.getY(), spawn.getZ() + 0.5d);
+		} else {
+			GameProfile profile = server.getPlayerProfileCache().getGameProfileForUsername(username);
+
+			if (profile != null && profile.isComplete()) {
+				FakePlayer fakePlayer = FakePlayerFactory.get(world, profile);
+				IPlayerFileData saveHandler = world.getSaveHandler().getPlayerNBTManager();
+				NBTTagCompound tag = saveHandler.readPlayerData(fakePlayer);
+
+				if (tag == null) {
+					return "Unknown player: " + username;
+				}
+
+				fakePlayer.dimension = 0;
+				fakePlayer.posX = spawn.getX() + 0.5d;
+				fakePlayer.posY = spawn.getY();
+				fakePlayer.posZ = spawn.getZ() + 0.5d;
+
+				saveHandler.writePlayerData(fakePlayer);
+			} else {
+				return "Unknown player: " + username;
+			}
+		}
+		return null;
+	}
+
+}
